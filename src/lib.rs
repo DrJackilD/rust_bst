7004b99207858c684fd40e01f919bed4ef23e6b2
drjackild <edubovskoy@gmail.com>
2020-02-25 23:06:07 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,101 @@
+const TRAVERSE_SYMBOL: &str = "│  ";
+const TWO_WAY_SYMBOL: &str = "├──";
+const ONE_WAY_SYMBOL: &str = "└──";
+
+pub struct Tree<T> {
+    root: Node<T>,
+}
+
+impl<T> Tree<T>
+where
+    T: std::fmt::Display,
+{
+    pub fn new(root: Node<T>) -> Tree<T> {
+        Tree { root }
+    }
+
+    pub fn show(&self) -> String {
+        let mut sb = String::new();
+
+        sb.push_str(&format!("{}", self.root));
+        let left_pointer = if self.root.right.is_some() {
+            TWO_WAY_SYMBOL
+        } else {
+            ONE_WAY_SYMBOL
+        };
+        match &self.root.left {
+            Some(left) => left.show(&mut sb, "", left_pointer, self.root.right.is_some()),
+            None => (),
+        }
+        match &self.root.right {
+            Some(right) => right.show(&mut sb, "", ONE_WAY_SYMBOL, false),
+            None => (),
+        }
+        sb
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Node<T> {
+    value: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T>
+where
+    T: std::fmt::Display,
+{
+    fn show(&self, sb: &mut String, padding: &str, pointer: &str, has_right_leaf: bool) {
+        sb.push_str(&format!("\n{}{}{}", padding, pointer, self));
+        let mut padding_str = String::from(padding);
+        match has_right_leaf {
+            true => padding_str.push_str(&format!("{}   ", TRAVERSE_SYMBOL)),
+            false => padding_str.push_str("   "),
+        }
+        let left_pointer = if self.right.is_some() {
+            TWO_WAY_SYMBOL
+        } else {
+            ONE_WAY_SYMBOL
+        };
+        match &self.left {
+            Some(left) => left.show(sb, &padding_str, left_pointer, self.right.is_some()),
+            None => (),
+        }
+        match &self.right {
+            Some(right) => right.show(sb, &padding_str, ONE_WAY_SYMBOL, false),
+            None => (),
+        }
+    }
+}
+
+impl<T> Node<T>
+where
+    T: std::default::Default,
+{
+    pub fn new(value: T) -> Node<T> {
+        Node {
+            value,
+            ..Default::default()
+        }
+    }
+
+    pub fn left(mut self, leaf: Node<T>) -> Self {
+        self.left = Some(Box::new(leaf));
+        self
+    }
+
+    pub fn right(mut self, leaf: Node<T>) -> Self {
+        self.right = Some(Box::new(leaf));
+        self
+    }
+}
+
+impl<T> std::fmt::Display for Node<T>
+where
+    T: std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}]", self.value)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use bst::{Node, Tree};
+
+fn main() {
+    let root = Node::new("root")
+        .left(
+            Node::new("node1")
+                .left(Node::new("node3"))
+                .right(Node::new("node4")),
+        )
+        .right(
+            Node::new("node2")
+                .left(
+                    Node::new("node5").left(
+                        Node::new("node7")
+                            .left(Node::new("node8"))
+                            .right(Node::new("node9")),
+                    ),
+                )
+                .right(Node::new("node6")),
+        );
+    let tree = Tree::new(root);
+    println!("{}", tree.show());
+}
